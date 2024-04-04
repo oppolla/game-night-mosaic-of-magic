feb8806c6b201f6be0ec1596e6e45cb7e6338d7e
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2024-04-04 17:17:23 -0400

    clean up recipes/kit - no more unseal/sealed box

--- Contents/mods/game-night-mtg/media/lua/server/gameNight - MTG recipes.lua
+++ Contents/mods/game-night-mtg/media/lua/server/gameNight - MTG recipes.lua
@@ -4,7 +4,7 @@ require "gameNight - recipes"
 ---@param items ArrayList
 ---@param result InventoryItem|InventoryContainer
 ---@param player IsoPlayer
-function Recipe.GameNight.UnboxMTG(items, result, player) result:getModData()["gameNight_specialOnCardApplyBoosters"] = 4 end
+function Recipe.GameNight.OpenKitMTG(items, result, player) result:getModData()["gameNight_specialOnCardApplyBoosters"] = 4 end
 
 ---@param items ArrayList
 ---@param result InventoryItem|InventoryContainer
