1f22c7b185642ff9bef2c0c83acfd24d79fc8816
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2024-04-05 14:51:12 -0400

    Update gameNight - DistributionsMTG.lua

--- Contents/mods/game-night-mtg/media/lua/server/gameNight - DistributionsMTG.lua
+++ Contents/mods/game-night-mtg/media/lua/server/gameNight - DistributionsMTG.lua
@@ -2,10 +2,41 @@ require "Items/SuburbsDistributions"
 
 local gameNightDistro = require "gameNight - Distributions"
 
---mtgStarterKit
---mtgBoosterPack
---mtgCards
+gameNightDistro.proceduralDistGameNight.itemsToAdd["mtgStarterKit"] = {
+    chanceFactor = 0.5,
+    perDistFactor = {
+        ["ClassroomDesk"] = 0.001,
+        ["BedroomDresser"] = 0.001,
+        ["ClassroomMisc"] = 0.001,
+        ["SchoolLockers"] = 0.001,
+        ["OfficeDeskHome"]=0,
+        ["BarCounterMisc"]=0,
+        ["Hobbies"]=0.001,
+        ["WardrobeChild"]=0.001,
+        ["CrateRandomJunk"]=0.001,
+    }
+}
 
---gameNightDistro.proceduralDistGameNight.itemsToAdd["mtgStarterKit"] = {chanceFactor = 0.5}
---gameNightDistro.proceduralDistGameNight.itemsToAdd["mtgBoosterPack"] = {chanceFactor = 0.5}
---gameNightDistro.proceduralDistGameNight.itemsToAdd["mtgCards"] = {chanceFactor = 0.5}
+gameNightDistro.proceduralDistGameNight.itemsToAdd["mtgBoosterPack"] = {
+    chanceFactor = 0.5,
+    perDistFactor = {
+        ["ClassroomDesk"] = 0.001,
+        ["BedroomDresser"] = 0.001,
+        ["ClassroomMisc"] = 0.001,
+        ["SchoolLockers"] = 0.001,
+        ["OfficeDeskHome"]=0,
+        ["BarCounterMisc"]=0,
+        ["Hobbies"]=0.001,
+        ["WardrobeChild"]=0.001,
+        ["CrateRandomJunk"]=0.001,
+    }
+}
+
+gameNightDistro.proceduralDistGameNight.itemsToAdd["mtgCards"] = {
+    chanceFactor = 0.5,
+    perDistFactor = {
+        ["Gifts"]=0,
+        ["GigamartToys"]=0,
+        ["HolidayStuff"]=0,
+    }
+}
