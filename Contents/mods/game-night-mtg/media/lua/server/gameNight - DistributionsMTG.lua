a96fbe6bb7294c37da962c99d53bfced5f603605
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2024-04-05 09:39:14 -0400

    Create gameNight - DistributionsMTG.lua

---
+++ Contents/mods/game-night-mtg/media/lua/server/gameNight - DistributionsMTG.lua
@@ -0,0 +1,11 @@
+require "Items/SuburbsDistributions"
+
+local gameNightDistro = require "gameNight - Distributions"
+
+--mtgStarterKit
+--mtgBoosterPack
+--mtgCards
+
+--gameNightDistro.proceduralDistGameNight.itemsToAdd["mtgStarterKit"] = {chanceFactor = 0.5}
+--gameNightDistro.proceduralDistGameNight.itemsToAdd["mtgBoosterPack"] = {chanceFactor = 0.5}
+--gameNightDistro.proceduralDistGameNight.itemsToAdd["mtgCards"] = {chanceFactor = 0.5}
