1727a710a04751dbece140e9caa36eadc4c79af5
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2024-05-06 01:25:37 -0400

    generatable artwork

--- Contents/mods/game-night-mtg/media/lua/client/gameNight - implementMTG_Alpha.lua
+++ Contents/mods/game-night-mtg/media/lua/client/gameNight - implementMTG_Alpha.lua
@@ -6,7 +6,7 @@ local MTG = {}
 
 MTG.alphaCards = { --"MTG Alpha "
 
-    ["Artifacts"] = {
+    ["Relics"] = {
         "Juggernaut", "Gauntlet of Might", "Glasses of Urza", "Jayemdae Tome", "Disrupting Scepter",
         "Conservator", "Sol Ring", "Sunglasses of Urza", "Wooden Sphere", "Black Lotus", "Mox Emerald",
         "Rod of Ruin", "Celestial Prism", "Throne of Bone", "Ivory Cup", "Winter Orb", "Mox Jet",
@@ -19,9 +19,9 @@ MTG.alphaCards = { --"MTG Alpha "
 
     ["Black"] = {
         "Demonic Attorney", "Nether Shadow", "Mind Twist", "Dark Ritual", "Scathe Zombies", "Darkpact",
-        "Pestilence", "Frozen Shade", "Cursed Land", "Raise Dead", "Black Knight", "Paralyze", "Nightmare",
+        "Pestilence", "Frozen Shade", "Cursed Domain", "Raise Dead", "Black Knight", "Paralyze", "Nightmare",
         "Hypnotic Specter", "Plague Rats", "Wall of Bone", "Gloom", "Will-O-The-Wisp", "Warp Artifact",
-        "Contract from Below", "World of Command", "Unholy Strength", "Animate Dead", "Nettling Imp",
+        "Contract from Below", "Word of Command", "Unholy Strength", "Animate Dead", "Nettling Imp",
         "Demonic Tutor", "Deathlace", "Demonic Hordes", "Howl from Beyond", "Bog Wraith", "Drain Life",
         "Royal Assassin", "Sengir Vampire", "Bad Moon", "Scavenging Ghoul", "Lich", "Zombie Master",
         "Weakness", "Simulacrum", "Lord of the Pit", "Deathgrip ", "Drudge Skeletons", "Terror",
@@ -30,12 +30,12 @@ MTG.alphaCards = { --"MTG Alpha "
 
     ["White"] = {
         "Death Ward", "Serra Angel", "Blessing", "Samite Healer", "Mesa Pegasus", "Holy Strength",
-        "Healing Salve", "Red Ward", "Lance", "Animate Wall", "Consecrate Land", "Righteousness",
-        "Savannah Lions", "Veteran Bodyguard", "Personal Incarnation", "Island Sanctuary",
+        "Healing Salve", "Red Ward", "Lance", "Animate Wall", "Consecrate Domain", "Righteousness",
+        "Savannah Lions", "Veteran Bodyguard", "Personal Incarnation", "Sanctuary",
         "Circle of Protection: Red", "Castle", "Holy Armor", "Wrath of God", "Wall of Swords",
         "Blaze of Glory", "Reverse Damage", "Blue Ward", "Circle of Protection: White",
         "Circle of Protection: Blue", "Balance", "Karma", "Pearled Unicorn", "Northern Paladin",
-        "Armageddon", "White Knight", "Ressurection", "White Ward", "Swords to Plowshares",
+        "Armageddon", "White Knight", "Resurrection", "White Ward", "Swords to Plowshares",
         "Farmstead", "Purelace", "Disenchant", "Green Ward", "Circle of Protection: Green",
         "Black Ward", "Conversion", "Benalish Hero", "Guardian Angel", "Crusade", "Circle of Protection: Black",
     },
@@ -45,7 +45,7 @@ MTG.alphaCards = { --"MTG Alpha "
         "Berserk", "Llanowar Elves", "Fog", "Grizzly Bears", "Wall of Ice", "Living Artifact", "War Mammoth",
         "Channel", "Craw Wurm", "Timber Wolves", "Regrowth", "Ironroot Treefolk", "Regeneration", "Kudzu",
         "Wall of Brambles", "Stream of Life", "Natural Selection", "Fastbond", "Verduran Enchantress",
-        "Tsunami", "Cockatrice", "Ice Storm", "Wall of Wood", "Shanodin Dryads", "Living Lands", "Giant Spider",
+        "Tsunami", "Cockatrice", "Ice Storm", "Wall of Wood", "Shanodin Dryads", "Living Domains", "Giant Spider",
         "Tranquility", "Scryb Sprites", "Instill Energy", "Fungusaur", "Birds of Paradise", "Hurricane",
         "Lifelace", "Camouflage", "Web", "Giant Growth", "Lure", "Wanderlust", "Force of Nature", "Elvish Archers",
     },
@@ -55,7 +55,7 @@ MTG.alphaCards = { --"MTG Alpha "
         "Time Walk", "Wall of Air", "Siren's Call", "Sleight of Mind", "Sea Serpent", "Pirate Ship",
         "Power Leak", "Jump", "Stasis", "Volcanic Eruption", "Psionic Blast", "Timetwister", "Water Elemental",
         "Unsummon", "Spell Blast", "Steal Artifact", "Control Magic", "Vesuvan Doppelganger", "Mana Short",
-        "Ancestral Recall", "Air Elemental", "Flight", "Animate Artifact", "Braingeyser", "Twiddle",
+        "Ancestral Recall", "Air Elemental", "Flight", "Animate Relic", "Braingeyser", "Twiddle",
         "Wall of Water", "Mahamoti Djinn", "Magical Hack", "Merfolk of the Pearl Trident", "Thoughtlace",
         "Lord of Atlantis", "Blue Elemental Blast", "Phantasmal Terrain", "Power Sink", "Invisibility",
         "Lifetap", "Feedback", "Counterspell", "Phantasmal Forces", "Copy Artifact",
@@ -68,29 +68,29 @@ MTG.alphaCards = { --"MTG Alpha "
         "Disintegrate", "Stone Giant", "Mana Flare", "False Orders", "Smoke", "Hurloon Minotaur",
         "Red Elemental Blast", "Gray Ogre", "Roc of Kher Ridges", "Chaoslace", "Dragon Whelp",
         "Wheel of Fortune", "Power Surge", "Raging River", "Manabarbs", "Goblin King", "Stone Rain",
-        "Goblin balloon Brigade", "Shatter", "Rock Hydra", "Firebreathing", "Two-Handed Giant of Foriys",
+        "Goblin balloon Brigade", "Shatter", "Rock Hydra", "Firebreathing", "Two-Headed Giant of Foriys",
         "Uthden Troll", "Dwarven Warriors", "Wall of Stone", "Hill Giant", "Dwarven Demolition Team",
     },
 
     ["Spells"] = {},
-    ["Special Land"] = {},
+    ["Special Domain"] = {},
 
-    ["Black Land"] = { "Swamp", "Swamp", "Swamp"},
-    ["White Land"] = { "Plains", "Plains", "Plains"},
-    ["Green Land"] = { "Forest", "Forest", "Forest"},
-    ["Blue Land"] = { "Island", "Island", "Island"},
-    ["Red Land"] = { "Mountain", "Mountain", "Mountain"},
+    ["Black Domain"] = { "Swamp", "Swamp", "Swamp"},
+    ["White Domain"] = { "Plains", "Plains", "Plains"},
+    ["Green Domain"] = { "Forest", "Forest", "Forest"},
+    ["Blue Domain"] = { "Island", "Island", "Island"},
+    ["Red Domain"] = { "Mountain", "Mountain", "Mountain"},
 
-    ["Blue White Land"] = { "Tundra" },
-    ["Black Blue Land"] = { "Underground Sea" },
-    ["White Black Land"] = { "Scrubland" },
-    ["Red Black Land"] = { "Badlands" },
-    ["Red White Land"] = { "Plateau" },
-    ["Green Red Land"] = { "Taiga" },
-    ["White Green Land"] = { "Savannah" },
-    ["Green Blue Land"] = { "Tropical Island" },
-    ["Black Green Land"] = { "Bayou" },
-    ["Blue Red Land"] = { "Volcanic Island" },
+    ["Blue White Domain"] = { "Tundra" },
+    ["Black Blue Domain"] = { "Underground Sea" },
+    ["White Black Domain"] = { "Scrubland" },
+    ["Red Black Domain"] = { "Badlands" },
+    ["Red White Domain"] = { "Plateau" },
+    ["Green Red Domain"] = { "Taiga" },
+    ["White Green Domain"] = { "Savannah" },
+    ["Green Blue Domain"] = { "Tropical Island" },
+    ["Black Green Domain"] = { "Bayou" },
+    ["Blue Red Domain"] = { "Volcanic Island" },
 
 }
 
@@ -98,22 +98,22 @@ MTG.catalogue = {}
 MTG.altNames = {}
 
 MTG.alphaRare = {
-    "Artifacts 2", "Artifacts 5", "Artifacts 8", "Artifacts 11", "Artifacts 16", "Artifacts 17", "Artifacts 18",
-    "Artifacts 19", "Artifacts 20", "Artifacts 22", "Artifacts 23", "Artifacts 28", "Artifacts 29", "Artifacts 35",
-    "Artifacts 36", "Artifacts 37", "Artifacts 38", "Artifacts 44", "Artifacts 45", "Black 2", "Black 3", "Black 6",
-    "Black 18", "Black 19", "Black 20", "Black 21", "Black 26", "Black 36", "Black Blue Land 1", "Black Green Land 1",
-    "Blue Red Land 1", "Blue 4", "Blue 7", "Blue 10", "Blue 12", "Blue 15", "Blue 16", "Blue 18", "Blue 25", "Blue 26",
-    "Blue 33", "Blue 34", "Blue 37", "Blue 46", "Blue White Land 1", "Green 5", "Green 16", "Green 20", "Green 23",
-    "Green 25", "Green 27", "Green 31", "Green 37", "Green 39", "Green 41", "Green 45", "Green Blue Land 1",
-    "Green Red Land 1", "Red 1", "Red 2", "Red 5", "Red 6", "Red 22", "Red 24", "Red 31", "Red 32", "Red 33",
-    "Red 34", "Red 35", "Red 39", "Red Black Land 1", "Red White Land 1", "White 3", "White 12", "White 13",
-    "White 14", "White 15", "White 20", "White 22", "White 27", "White 31", "White Black Land 1", "White Green Land 1",
+    "Relics 2", "Relics 5", "Relics 8", "Relics 11", "Relics 16", "Relics 17", "Relics 18",
+    "Relics 19", "Relics 20", "Relics 22", "Relics 23", "Relics 28", "Relics 29", "Relics 35",
+    "Relics 36", "Relics 37", "Relics 38", "Relics 44", "Relics 45", "Black 2", "Black 3", "Black 6",
+    "Black 18", "Black 19", "Black 20", "Black 21", "Black 26", "Black 36", "Black Blue Domain 1", "Black Green Domain 1",
+    "Blue Red Domain 1", "Blue 4", "Blue 7", "Blue 10", "Blue 12", "Blue 15", "Blue 16", "Blue 18", "Blue 25", "Blue 26",
+    "Blue 33", "Blue 34", "Blue 37", "Blue 46", "Blue White Domain 1", "Green 5", "Green 16", "Green 20", "Green 23",
+    "Green 25", "Green 27", "Green 31", "Green 37", "Green 39", "Green 41", "Green 45", "Green Blue Domain 1",
+    "Green Red Domain 1", "Red 1", "Red 2", "Red 5", "Red 6", "Red 22", "Red 24", "Red 31", "Red 32", "Red 33",
+    "Red 34", "Red 35", "Red 39", "Red Black Domain 1", "Red White Domain 1", "White 3", "White 12", "White 13",
+    "White 14", "White 15", "White 20", "White 22", "White 27", "White 31", "White Black Domain 1", "White Green Domain 1",
 }
 
 MTG.alphaUncommon = {
-    "Artifacts 1", "Artifacts 6", "Artifacts 7", "Artifacts 8", "Artifacts 9", "Artifacts 12", "Artifacts 13",
-    "Artifacts 14", "Artifacts 15", "Artifacts 21", "Artifacts 25", "Artifacts 26", "Artifacts 27", "Artifacts 30",
-    "Artifacts 33", "Artifacts 34", "Artifacts 39", "Artifacts 40", "Artifacts 41", "Artifacts 43", "Black 9",
+    "Relics 1", "Relics 6", "Relics 7", "Relics 8", "Relics 9", "Relics 12", "Relics 13",
+    "Relics 14", "Relics 15", "Relics 21", "Relics 25", "Relics 26", "Relics 27", "Relics 30",
+    "Relics 33", "Relics 34", "Relics 39", "Relics 40", "Relics 41", "Relics 43", "Black 9",
     "Black 11", "Black 14", "Black 16", "Black 17", "Black 23", "Black 24", "Black 25", "Black 29", "Black 32",
     "Black 34", "Black 38", "Black 40", "Black 43", "Black 45", "Blue 2", "Blue 8", "Blue 9", "Blue 17", "Blue 19",
     "Blue 22", "Blue 23", "Blue 27", "Blue 29", "Blue 32", "Blue 42", "Blue 43", "Blue 44", "Blue 45", "Green 3",
@@ -135,14 +135,14 @@ MTG.alphaCommon = {
     "White 25", "White 26", "White 29", "White 38", "White 40", "White 43", "White 44",
 }
 
-MTG.alphaLand = {
-    "Red Land 1", "Red Land 2", "Red Land 3", "Blue Land 1", "Blue Land 2", "Blue Land 3",
-    "Green Land 1", "Green Land 2", "Green Land 3", "White Land 1", "White Land 2", "White Land 3",
-    "Black Land 1", "Black Land 2", "Black Land 3",
+MTG.alphaDomain = {
+    "Red Domain 1", "Red Domain 2", "Red Domain 3", "Blue Domain 1", "Blue Domain 2", "Blue Domain 3",
+    "Green Domain 1", "Green Domain 2", "Green Domain 3", "White Domain 1", "White Domain 2", "White Domain 3",
+    "Black Domain 1", "Black Domain 2", "Black Domain 3",
 }
 
 
-MTG.colorCodedRarity = {Land={}, Common={}, Uncommon={}, Rare={}}
+MTG.colorCodedRarity = {Domain={}, Common={}, Uncommon={}, Rare={}}
 
 --- Build entire catalogue as a deck
 for set,cards in pairs(MTG.alphaCards) do
@@ -171,8 +171,8 @@ deckActionHandler.addDeck("mtgCards", MTG.catalogue, MTG.altNames)
 
 applyItemDetails.MTG = {}
 
-function applyItemDetails.MTG.rollLand(rarity)
-    --The chance of getting a basic land instead of another card is approximately:
+function applyItemDetails.MTG.rollDomain(rarity)
+    --The chance of getting a basic Domain instead of another card is approximately:
     -- 4.13% for rares, 21.5% for uncommon and 38.84% for commons.
     local chance = rarity and (rarity == "Rare" and 4.13 or rarity == "Uncommon" and 21.5 or rarity == "Common" and 38.84)
     if chance then
@@ -203,14 +203,14 @@ end
 
 
 function applyItemDetails.MTG.rollCard(rarity)
-    --roll for land first
-    local rollLand = applyItemDetails.MTG.rollLand(rarity)
-    if rollLand then
+    --roll for Domain first
+    local rollDomain = applyItemDetails.MTG.rollDomain(rarity)
+    if rollDomain then
         if rarity == "Rare" then
-            -- The only lands on the rare sheets were five copies of Island.
-            return ("MTG Alpha Blue Land "..ZombRand(3)+1)
+            -- The only Domains on the rare sheets were five copies of Island.
+            return ("MTG Alpha Blue Domain "..ZombRand(3)+1)
         else
-            return ("MTG Alpha "..MTG.alphaLand[ZombRand(#MTG.alphaLand)+1])
+            return ("MTG Alpha "..MTG.alphaDomain[ZombRand(#MTG.alphaDomain)+1])
         end
     end
 
@@ -336,21 +336,21 @@ function MTG.buildDeck(archetype)
     --[[DEBUG]] local colorString = ""
     for i,c in ipairs(colors) do colorString = colorString..c..((#colors>1 and i~=#colors) and "/" or "") end
     --[[DEBUG]] local rarities = {Common=0, Uncommon = 0, Rare =0}
-    --avg goal of 6 artifacts
+    --avg goal of 6 Relics
     --11 instead of 10 skews the average lower
     local artifactGoal = math.floor(deckSize/11)+ZombRand(3) -- 0 to 2 additional
     for i=1, artifactGoal do
         local rarity = applyItemDetails.MTG.weighedProbability({ Uncommon = 3, Rare = 1})
         --[[DEBUG]] rarities[rarity] = rarities[rarity]+1
-        local card = applyItemDetails.MTG.rollCardOfParticularColor(rarity, "Artifacts")
+        local card = applyItemDetails.MTG.rollCardOfParticularColor(rarity, "Relics")
         table.insert(cards, card)
     end
 
-    --avg goal of 24 land
-    local landGoal = math.floor(deckSize/2.5)+ZombRand(4) --0 to 3
-    for i=1, landGoal do
+    --avg goal of 24 Domain
+    local DomainGoal = math.floor(deckSize/2.5)+ZombRand(4) --0 to 3
+    for i=1, DomainGoal do
         local color = colors[ZombRand(#colors)+1]
-        local card = applyItemDetails.MTG.rollCardOfParticularColor("Land", color.." Land")
+        local card = applyItemDetails.MTG.rollCardOfParticularColor("Domain", color.." Domain")
         table.insert(cards, card)
     end
 
@@ -364,8 +364,8 @@ function MTG.buildDeck(archetype)
     end
 
     if getDebug() then
-        print("DECK BUILT: ", " c:"..#cards, " +a:"..artifactGoal, " +l:"..landGoal, " +o:"..remainingCount,
-                "=("..remainingCount+landGoal+artifactGoal..")",
+        print("DECK BUILT: ", " c:"..#cards, " +a:"..artifactGoal, " +l:"..DomainGoal, " +o:"..remainingCount,
+                "=("..remainingCount+DomainGoal+artifactGoal..")",
                 " (r:"..rarities.Rare," u:"..rarities.Uncommon," c:"..rarities.Common..")",
                 "  ["..colorString.."]")
     end
