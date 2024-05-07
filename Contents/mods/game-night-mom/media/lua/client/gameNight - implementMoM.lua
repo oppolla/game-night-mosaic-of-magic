f7c0bd51512abff16e9779fae38708dfd3bc83fe
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2024-05-07 19:15:43 -0400

    Update gameNight - implementMoM.lua

--- Contents/mods/game-night-mom/media/lua/client/gameNight - implementMoM.lua
+++ Contents/mods/game-night-mom/media/lua/client/gameNight - implementMoM.lua
@@ -4,83 +4,83 @@ local gamePieceAndBoardHandler = applyItemDetails.gamePieceAndBoardHandler
 
 local MOM = {}
 
-MOM.alphaCards = { --"MOM Alpha "
+MOM.Cards = {
     ["Black"] = {
-        "Cross-Roads Demon",	"Night Shadow",	"Hex",	"Cult Ritual",	"Zombie Horde",
-        "Darkpact",	"Pestilence",	"Cold-Handed Shade",	"Cursed Domain",	"Necromancy",
-        "Black Knight",	"Shudder",	"Night Mare",	"Hypnotic Specter",	"Infested Vermin",
-        "Wall of Skulls",	"Dread",	"Revenant",	"Warp Relic",	"Cultist's Contract",
-        "Dark Commandment",	"Unholy Strength",	"Animate Dead",	"Taunting Imp",	"Cultistish Summon",
-        "Cult Sigil",	"Cult Gathering",	"Deathwhistle",	"Bog-Body",	"Drain Soul",
-        "Cursed Assassin",	"Sanguine Vampire",	"Full Moon",	"Carrion Ghoul",	"Lich",
-        "Zombie Master",	"Weakness",	"Tribute",	"Abyssal Ravager",	"Verdant Decay",
-        "Skeleton Horde",	"Terror",	"Sacrifice",	"Fear",	"Evil Presence",
+        "Cross-Roads Demon", "Night Shadow", "Hex", "Cult Ritual", "Zombie Horde",
+        "Darkpact", "Pestilence", "Cold-Handed Shade", "Cursed Domain", "Necromancy",
+        "Black Knight", "Shudder", "Night Mare", "Hypnotic Specter", "Infested Vermin",
+        "Wall of Skulls", "Dread", "Revenant", "Warp Relic", "Cultist's Contract",
+        "Dark Commandment", "Unholy Strength", "Animate Dead", "Taunting Imp", "Cultistish Summon",
+        "Cult Sigil", "Cult Gathering", "Deathwhistle", "Bog-Body", "Drain Soul",
+        "Cursed Assassin", "Sanguine Vampire", "Full Moon", "Carrion Ghoul", "Lich",
+        "Zombie Master", "Weakness", "Tribute", "Abyssal Ravager", "Verdant Decay",
+        "Skeleton Horde", "Terror", "Sacrifice", "Fear", "Evil Presence",
         "Sinkhole",
         },
 
     ["White"] = {
-    "Death Ward",	"Serra Angel",	"Blessing",	"Samite Healer",	"Mesa Pegasus",
-    "Holy Strength",	"Healing Salve",	"Red Ward",	"Lance",	"Animate Wall",
-    "Consecrate Domain",	"Righteousness",	"Savannah Lions",	"Veteran Bodyguard",	"Personal Incarnation",
-    "Sanctuary",	"Sigil of Defense: Red",	"Castle",	"Holy Armor",	"Wrath of God",
-    "Wall of Swords",	"Blaze of Glory",	"Reverse Damage",	"Blue Ward",	"Sigil of Defense: White",
-    "Sigil of Defense: Blue",	"Balance",	"Karma",	"Pearled Unicorn",	"Northern Paladin",
-    "Armageddon",	"White Knight",	"Resurrection",	"White Ward",	"Swords to Plowshares",
-    "Farmstead",	"Divine Sigil",	"Disempower",	"Green Ward",	"Sigil of Defense: Green",
-    "Black Ward",	"Conversion",	"Benalish Hero",	"Guardian Angel",	"Crusade",
-    "Sigil of Defense: Black",
+        "Death Ward", "Serra Angel", "Blessing", "Samite Healer", "Mesa Pegasus",
+        "Holy Strength", "Healing Salve", "Red Ward", "Lance", "Animate Wall",
+        "Consecrate Domain", "Righteousness", "Savannah Lions", "Veteran Bodyguard", "Personal Incarnation",
+        "Sanctuary", "Sigil of Defense: Red", "Castle", "Holy Armor", "Wrath of God",
+        "Wall of Swords", "Blaze of Glory", "Reverse Damage", "Blue Ward", "Sigil of Defense: White",
+        "Sigil of Defense: Blue", "Balance", "Karma", "Pearled Unicorn", "Northern Paladin",
+        "Armageddon", "White Knight", "Resurrection", "White Ward", "Swords to Plowshares",
+        "Farmstead", "Divine Sigil", "Disempower", "Green Ward", "Sigil of Defense: Green",
+        "Black Ward", "Conversion", "Benalish Hero", "Guardian Angel", "Crusade",
+        "Sigil of Defense: Black",
     },
 
     ["Green"] = {
-    "Spirit of The Fang",	"Wild Growth",	"Deep-Wood Druid",	"Bush Viper",	"Gaea's Liege",
-    "Lifeforce Channeling",	"Berserk",	"Deep-Forest Elves",	"Fog",	"Grizzly Bears",
-    "Wall of Ice",	"Living Relic",	"War Mammoth",	"Mystic Channeling",	"Wyrm",
-    "Timber Wolves",	"Regrowth",	"Ironroot Treefolk",	"Regeneration",	"Kudzu",
-    "Wall of Brambles",	"Stream of Life",	"Natural Selection",	"Fastbond",	"Verduran Empowerer",
-    "Tsunami",	"Cockatrice",	"Ice Storm",	"Wall of Wood",	"Deep Dryads",
-    "Living Domains",	"Giant Spider",	"Tranquility",	"Scryb Sprites",	"Instill Energy",
-    "Mushroom Sentinal",	"Birds of Paradise",	"Hurricane",	"Druid Sigil",	"Verdant Veil",
-    "Web",	"Giant Growth",	"Lure",	"Wanderlust",	"Force of Nature",
-    "Elvish Archers",
+        "Spirit of The Fang", "Wild Growth", "Deep-Wood Druid", "Bush Viper", "Gaea's Liege",
+        "Lifeforce Channeling", "Berserk", "Deep-Forest Elves", "Fog", "Grizzly Bears",
+        "Wall of Ice", "Living Relic", "War Mammoth", "Mystic Channeling", "Wyrm",
+        "Timber Wolves", "Regrowth", "Ironroot Treefolk", "Regeneration", "Kudzu",
+        "Wall of Brambles", "Stream of Life", "Natural Selection", "Fastbond", "Verduran Empowerer",
+        "Tsunami", "Cockatrice", "Ice Storm", "Wall of Wood", "Deep Dryads",
+        "Living Domains", "Giant Spider", "Tranquility", "Scryb Sprites", "Instill Energy",
+        "Mushroom Sentinal", "Birds of Paradise", "Hurricane", "Druid Sigil", "Verdant Veil",
+        "Web", "Giant Growth", "Lure", "Wanderlust", "Force of Nature",
+        "Elvish Archers",
     },
 
     ["Red"] = {
-    "Greater Dragon",	"Granite Gargoyle",	"Wall of Fire",	"Flashfires",	"Fork",
-    "Earthquake",	"Fire Elemental",	"Lightning Bolt",	"Ironjaw Ogres",	"Fireball",
-    "Dog-Goblin Raiders",	"Earth Elemental",	"Tunnel",	"Sedge Troll",	"Dog-Goblin Artillery",
-    "Earthbind",	"Burrowing",	"Dog-Goblin Herald",	"Deep-Earth Warlord",	"Disintegrate",
-    "Stone Giant",	"Arcana Flare",	"Forged Decree",	"Smoke",	"Deep-Rock Labyrinth Minotaur",
-    "Red Elemental Blast",	"Gray Ogre",	"Eagle of Red-Ridges",	"Obsidian Sigil",	"Dragon Whelp",
-    "Wheel of Fortune",	"Power Surge",	"Raging Currents",	"Arcanabarbs",	"Dog-Goblin King",
-    "Rockslide",	"Dog-Goblin Balloon Brigade",	"Shatter",	"Rock-Eating Worm",	"Firebreathing",
-    "One-Handed Giant of Deep-Rock",	"Deep-Rock Troll",	"Dwarven Warriors",	"Wall of Stone",	"Hill Giant",
-    "Dwarven Demolition Team",
+        "Greater Dragon", "Granite Gargoyle", "Wall of Fire", "Flashfires", "Fork",
+        "Earthquake", "Fire Elemental", "Lightning Bolt", "Ironjaw Ogres", "Fireball",
+        "Dog-Goblin Raiders", "Earth Elemental", "Tunnel", "Sedge Troll", "Dog-Goblin Artillery",
+        "Earthbind", "Burrowing", "Dog-Goblin Herald", "Deep-Earth Warlord", "Disintegrate",
+        "Stone Giant", "Arcana Flare", "Forged Decree", "Smoke", "Deep-Rock Labyrinth Minotaur",
+        "Red Elemental Blast", "Gray Ogre", "Eagle of Red-Ridges", "Obsidian Sigil", "Dragon Whelp",
+        "Wheel of Fortune", "Power Surge", "Raging Currents", "Arcanabarbs", "Dog-Goblin King",
+        "Rockslide", "Dog-Goblin Balloon Brigade", "Shatter", "Rock-Eating Worm", "Firebreathing",
+        "One-Handed Giant of Deep-Rock", "Deep-Rock Troll", "Dwarven Warriors", "Wall of Stone", "Hill Giant",
+        "Dwarven Demolition Team",
     },
 
     ["Blue"] = {
-    "Mindbreaker Toxin",	"Clone",	"Being Bond",	"Power Release",	"Prodigal Sorcerer",
-    "Phantom Monster",	"Time Warp",	"Wall of Air",	"Siren's Call",	"Memory Forgery",
-    "Sea Serpent",	"Pirate Ship",	"Power Leak",	"Jump",	"Stasis",
-    "Volcanic Eruption",	"Psionic Blast",	"Timetwister",	"Water Elemental",	"Unsummon",
-    "Spell Blast",	"Steal Relic",	"Control Magic",	"Doppelganger",	"Arcana Short",
-    "Ancestral Recall",	"Air Elemental",	"Flight",	"Animate Relic",	"Intense Clarity",
-    "Twiddle",	"Wall of Water",	"Djinn",	"Magical Hack",	"Fishman of the Shattered Trident",
-    "Arcana Sigil",	"Lord of The Sunken",	"Blue Elemental Blast",	"Wild-Arcane Terrain",	"Power Gamble",
-    "Invisibility",	"Life Channel",	"Feedback",	"Counterspell",	"Wild-Arcane Forces",
-    "Copy Relic",
+        "Mindbreaker Toxin", "Clone", "Being Bond", "Power Release", "Prodigal Sorcerer",
+        "Phantom Monster", "Time Warp", "Wall of Air", "Siren's Call", "Memory Forgery",
+        "Sea Serpent", "Pirate Ship", "Power Leak", "Jump", "Stasis",
+        "Volcanic Eruption", "Psionic Blast", "Timetwister", "Water Elemental", "Unsummon",
+        "Spell Blast", "Steal Relic", "Control Magic", "Doppelganger", "Arcana Short",
+        "Ancestral Recall", "Air Elemental", "Flight", "Animate Relic", "Intense Clarity",
+        "Twiddle", "Wall of Water", "Djinn", "Magical Hack", "Fishman of the Shattered Trident",
+        "Arcana Sigil", "Lord of The Sunken", "Blue Elemental Blast", "Wild-Arcane Terrain", "Power Gamble",
+        "Invisibility", "Life Channel", "Feedback", "Counterspell", "Wild-Arcane Forces",
+        "Copy Relic",
     },
 
     ["Relics"] = {
-    "Dwarven-Crafted Golem",	"Totem of Deep-Rock Forge",	"Stolen God Eye",	"Jayemdae Tome",	"Disrupting Scepter",
-    "Conservator",	"Sol Ring",	"Sunglasses of Urza",	"Wooden Sphere",	"Blackjack Jack o' Lantern",
-    "Alchemic Formula: Emerald",	"Forgotten Sigil of Ruin",	"Chromatic Prism",	"Comandments of Bone",	"Divine Chalice",
-    "Orb of Stagnation",	"Alchemic Formula: Onyx",	"Bottled Unstable Wild-Arcana",	"Time Shift",	"Alchemic Formula: Pearl",
-    "Emerald Effigy",	"Totem of The Swamp",	"Alchemic Formula: Ruby",	"Alchemic Formula: Sapphire",	"Collar of Avarice",
-    "Arcana-Reactive-Quartz Rod",	"Obsidian Obelisk",	"Totem of The Goddess",	"Phantom Veil",	"Reactive Arcana-Anvil",
-    "Spritenest",	"Solid Gold Finger",	"Ancient Golem",	"Living Wall",	"Hooked Curse",
-    "Kormus Bell",	"Forcefield",	"Helm of Chatzuk",	"Copper Tablet",	"Soul Net",
-    "Icy Manipulator",	"Clockwork Beast",	"Library of Leng",	"Arcana Vault",	"Meekstone",
-    "Nevinyrral's Disk",	"Ankh of Mishra",
+        "Dwarven-Crafted Golem", "Totem of Deep-Rock Forge", "Stolen God Eye", "Jayemdae Tome", "Disrupting Scepter",
+        "Conservator", "Sol Ring", "Sunglasses of Urza", "Wooden Sphere", "Blackjack Jack o' Lantern",
+        "Alchemic Formula: Emerald", "Forgotten Sigil of Ruin", "Chromatic Prism", "Comandments of Bone", "Divine Chalice",
+        "Orb of Stagnation", "Alchemic Formula: Onyx", "Bottled Unstable Wild-Arcana", "Time Shift", "Alchemic Formula: Pearl",
+        "Emerald Effigy", "Totem of The Swamp", "Alchemic Formula: Ruby", "Alchemic Formula: Sapphire", "Collar of Avarice",
+        "Arcana-Reactive-Quartz Rod", "Obsidian Obelisk", "Totem of The Goddess", "Phantom Veil", "Reactive Arcana-Anvil",
+        "Spritenest", "Solid Gold Finger", "Ancient Golem", "Living Wall", "Hooked Curse",
+        "Kormus Bell", "Forcefield", "Helm of Chatzuk", "Copper Tablet", "Soul Net",
+        "Icy Manipulator", "Clockwork Beast", "Library of Leng", "Arcana Vault", "Meekstone",
+        "Nevinyrral's Disk", "Ankh of Mishra",
     },
 
     ["Black Domain"] = { "Swamp", "Swamp", "Swamp"},
@@ -105,7 +105,7 @@ MOM.alphaCards = { --"MOM Alpha "
 MOM.catalogue = {}
 MOM.altNames = {}
 
-MOM.alphaRare = {
+MOM.Rare = {
     "Relics 2", "Relics 5", "Relics 8", "Relics 11", "Relics 16", "Relics 17", "Relics 18",
     "Relics 19", "Relics 20", "Relics 22", "Relics 23", "Relics 28", "Relics 29", "Relics 35",
     "Relics 36", "Relics 37", "Relics 38", "Relics 44", "Relics 45", "Black 2", "Black 3", "Black 6",
@@ -118,7 +118,7 @@ MOM.alphaRare = {
     "White 14", "White 15", "White 20", "White 22", "White 27", "White 31", "White Black Domain 1", "White Green Domain 1",
 }
 
-MOM.alphaUncommon = {
+MOM.Uncommon = {
     "Relics 1", "Relics 6", "Relics 7", "Relics 8", "Relics 9", "Relics 12", "Relics 13",
     "Relics 14", "Relics 15", "Relics 21", "Relics 25", "Relics 26", "Relics 27", "Relics 30",
     "Relics 33", "Relics 34", "Relics 39", "Relics 40", "Relics 41", "Relics 43", "Black 9",
@@ -132,7 +132,7 @@ MOM.alphaUncommon = {
     "White 34", "White 35", "White 39", "White 41", "White 42",
 }
 
-MOM.alphaCommon = {
+MOM.Common = {
     "Black 4", "Black 5", "Black 7", "Black 8", "Black 10", "Black 12", "Black 15", "Black 22", "Black 30",
     "Black 37", "Black 41", "Black 42", "Black 44", "Black 46", "Blue 1", "Blue 3", "Blue 5", "Blue 11",
     "Blue 13", "Blue 14", "Blue 20", "Blue 21", "Blue 28", "Blue 31", "Blue 35", "Blue 38", "Blue 39",
@@ -143,7 +143,7 @@ MOM.alphaCommon = {
     "White 25", "White 26", "White 29", "White 38", "White 40", "White 43", "White 44",
 }
 
-MOM.alphaDomain = {
+MOM.Domain = {
     "Red Domain 1", "Red Domain 2", "Red Domain 3", "Blue Domain 1", "Blue Domain 2", "Blue Domain 3",
     "Green Domain 1", "Green Domain 2", "Green Domain 3", "White Domain 1", "White Domain 2", "White Domain 3",
     "Black Domain 1", "Black Domain 2", "Black Domain 3",
@@ -153,15 +153,15 @@ MOM.alphaDomain = {
 MOM.colorCodedRarity = {Domain={}, Common={}, Uncommon={}, Rare={}}
 
 --- Build entire catalogue as a deck
-for set,cards in pairs(MOM.alphaCards) do
+for set,cards in pairs(MOM.Cards) do
     for i,card in pairs(cards) do
-        local cardID = "MOM Alpha "..set.." "..i
+        local cardID = set.." "..i
         MOM.altNames[cardID] = card
 
         local keyed = false
         for rarity,data in pairs(MOM.colorCodedRarity) do
             if not keyed then
-                for n,c in pairs(MOM["alpha"..rarity]) do
+                for n,c in pairs(MOM[rarity]) do
                     if c == (set.." "..i) then
                         MOM.colorCodedRarity[rarity][set] = MOM.colorCodedRarity[rarity][set] or {}
                         table.insert(MOM.colorCodedRarity[rarity][set], c)
@@ -206,24 +206,15 @@ end
 
 function applyItemDetails.MOM.rollCardOfParticularColor(rarity, set)
     local cardPool = MOM.colorCodedRarity[rarity][set]
-    return ("MOM Alpha "..cardPool[ZombRand(#cardPool)+1])
+    return (cardPool[ZombRand(#cardPool)+1])
 end
 
 
 function applyItemDetails.MOM.rollCard(rarity)
-    --roll for Domain first
     local rollDomain = applyItemDetails.MOM.rollDomain(rarity)
-    if rollDomain then
-        if rarity == "Rare" then
-            -- The only Domains on the rare sheets were five copies of Island.
-            return ("MOM Alpha Blue Domain "..ZombRand(3)+1)
-        else
-            return ("MOM Alpha "..MOM.alphaDomain[ZombRand(#MOM.alphaDomain)+1])
-        end
-    end
-
-    local cardPool = MOM["alpha"..rarity]
-    return ("MOM Alpha "..cardPool[ZombRand(#cardPool)+1])
+    if rollDomain then return (MOM.Domain[ZombRand(#MOM.Domain)+1]) end
+    local cardPool = MOM[rarity]
+    return (cardPool[ZombRand(#cardPool)+1])
 end
 
 
