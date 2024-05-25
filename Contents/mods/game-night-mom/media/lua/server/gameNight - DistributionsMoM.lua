6ee0bcbe9d1a0748a61a95c3deab759a1768eb87
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2024-05-25 18:24:30 -0400

    Update gameNight - DistributionsMoM.lua

--- Contents/mods/game-night-mom/media/lua/server/gameNight - DistributionsMoM.lua
+++ Contents/mods/game-night-mom/media/lua/server/gameNight - DistributionsMoM.lua
@@ -3,7 +3,12 @@ require "Items/SuburbsDistributions"
 local gameNightDistro = require "gameNight - Distributions"
 
 gameNightDistro.proceduralDistGameNight.itemsToAdd["momStarterKit"] = {
+    rolls = 6,
     perDistFactor = {
+        ["Gifts"] = 1.5,
+        ["GigamartToys"] = 1.5,
+        ["HolidayStuff"] = 1.5,
+
         ["ClassroomDesk"] = 0.01,
         ["BedroomDresser"] = 0.01,
         ["ClassroomMisc"] = 0.01,
@@ -17,7 +22,12 @@ gameNightDistro.proceduralDistGameNight.itemsToAdd["momStarterKit"] = {
 }
 
 gameNightDistro.proceduralDistGameNight.itemsToAdd["momBoosterPack"] = {
+    rolls = 12,
     perDistFactor = {
+        ["Gifts"] = 1.5,
+        ["GigamartToys"] = 1.5,
+        ["HolidayStuff"] = 1.5,
+
         ["ClassroomDesk"] = 0.01,
         ["BedroomDresser"] = 0.01,
         ["ClassroomMisc"] = 0.01,
@@ -31,11 +41,12 @@ gameNightDistro.proceduralDistGameNight.itemsToAdd["momBoosterPack"] = {
 }
 
 gameNightDistro.proceduralDistGameNight.itemsToAdd["momCards"] = {
+    rolls = 2,
     perDistFactor = {
         ["Gifts"]=0,
         ["GigamartToys"]=0,
         ["HolidayStuff"]=0,
-        ["SchoolLockers"] = 1.5,
-        ["ClassroomDesk"] = 1.5,
+        ["SchoolLockers"] = 1.75,
+        ["ClassroomDesk"] = 1.75,
     }
 }
