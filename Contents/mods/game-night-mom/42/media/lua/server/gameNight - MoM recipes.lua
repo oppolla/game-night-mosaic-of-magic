010b9f618d01f3fec6140934d975ed9282841b10
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2025-02-01 12:52:57 -0500

    update recipes

--- Contents/mods/game-night-mom/42/media/lua/server/gameNight - MoM recipes.lua
+++ Contents/mods/game-night-mom/42/media/lua/server/gameNight - MoM recipes.lua
@@ -2,7 +2,7 @@ require "recipecode"
 require "gameNight - recipes"
 
 
-function Recipe.GameNight.OpenKitMoM(craftRecipeData, character)
+function Recipe.GameNight.OpenSealedMoMCards(craftRecipeData, character)
 
     local item = craftRecipeData:getAllConsumedItems():get(0)
     local result = craftRecipeData:getAllCreatedItems():get(0)
@@ -14,21 +14,5 @@ function Recipe.GameNight.OpenKitMoM(craftRecipeData, character)
     end
 
     result:getModData()["gameNight_specialOnCardApplyBoosterSet"] = item:getModData()["gameNight_specialOnCardApplyBoosterSet"]
-    result:getModData()["gameNight_specialOnCardApplyBoosterCount"] = 4
-end
-
-
-function Recipe.GameNight.OpenBoosterMoM(craftRecipeData, character)
-
-    local item = craftRecipeData:getAllConsumedItems():get(0)
-    local result = craftRecipeData:getAllCreatedItems():get(0)
-
-    local prebuiltID = item:getModData()["gameNight_momPrebuilt"]
-    if prebuiltID then
-        result:getModData()["gameNight_momPrebuilt"] = prebuiltID
-        return
-    end
-
-    result:getModData()["gameNight_specialOnCardApplyBoosterSet"] = item:getModData()["gameNight_specialOnCardApplyBoosterSet"]
-    result:getModData()["gameNight_specialOnCardApplyBoosterCount"] = 1
+    result:getModData()["gameNight_specialOnCardApplyBoosterCount"] = item:getModData()["gameNight_specialOnCardApplyBoosterCount"]
 end
